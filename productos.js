bc0bc09dc547bb979d5fdd75a9364c3675f80cb3
leandro rodriguez <leanjavier@gmail.com>
2023-08-15 00:06:17 -0300

    se modifican nombre de vinos

--- productos.js
+++ productos.js
@@ -1,7 +1,7 @@
 const vinos = [
     { id: 1, nombre: "Gran Enemigo", precio: 20000 },
     { id: 2, nombre: "Contracara Reserva Malbec", precio: 18000 },
-    { id: 3, nombre: "Zorzal Gran Terroir Cabernet Franc", precio: 12000 },
+    { id: 3, nombre: "Mano Negra", precio: 12000 },
     { id: 4, nombre: "Puna 2600 Malbec", precio: 8500 },
     { id: 5, nombre: "La Posta Pizzella Malbec", precio: 13600 },
     { id: 6, nombre: "Alto Cedro", precio: 15000 },
